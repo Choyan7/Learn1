0546590c4bae1e2141211249fbdb570bb068764f
Choyan7 <75743622+Choyan7@users.noreply.github.com>
2021-01-27 19:42:52 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    
+    $('.video-link').magnificPopup({
+        type: 'image'
+        // other options
+    });
+	
+	$('.counter1').counterUp({
+        delay: 10,
+        time: 1000
+    });
+    
+    $('#mobile-menu').slicknav({
+    });
+	
+	
+	
+	
+})
